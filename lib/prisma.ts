b1cb94454caf9a2a0a638de160c2abf391fd1c1a
samir7888 <basnetsameer78@gmail.com>
2026-04-28 22:54:40 +0545

    fixed prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,11 @@
-import { PrismaClient } from '@/app/generated/prisma/client'
+import { PrismaClient } from '../app/generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-function createPrismaClient() {
+function createPrismaClient() { 
   const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL!,
   })
